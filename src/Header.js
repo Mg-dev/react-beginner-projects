298a76210620285b789764e4f25edf6ca3f7669a
Mg-dev <tyt22yearsold@gmail.com>
2023-02-06 15:28:53 +0630

    teamGroupMember

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import './App.css';
 const Header = ({selectedTeam, teamMemberCount }) => {
     return (
-        <div className='container'>
-            <div className='col-6 offset-3'>
-            <h4><u>Team Member Allocation</u></h4>
+        <div>
+            <div className=' text-center'>
+            <h5 className=''><u>Team Member Allocation</u></h5>
             <p className=' text-primary'>{selectedTeam} has <strong style={{color : 'black'}}> {teamMemberCount} </strong> member</p>
             </div>
         </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,22 +3,27 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg bg-primary">
-        <div class="container-fluid d-block">
+        <div class="container-fluid">
             <Link to="/"><span class="navbar-brand text-white">Logo</span></Link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <div>
+            <button class="navbar-toggler float-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse float-end" id="navbarNav">
-            <ul class="navbar-nav">
+            </div>
+            <div class="collapse navbar-collapse text-center" id="navbarNav">
+            <ul class="navbar-nav float-end ms-3">
                 <li class="nav-item">
-                <Link class="navbar-brand" to="/"><small className="text-light fs-8" style={{fontSize: '14px'}}>Home</small></Link>
+                <Link class="navbar-brand" to="/"><small className="text-light" style={{fontSize: '14px'}}>Home</small></Link>
                 </li>
                 <li class="nav-item">
-                <Link class="navbar-brand" to="/tma-app"><small className="text-light" style={{fontSize: '14px'}}>Team Member Allocation App</small></Link>
+                <Link class="navbar-brand" to="/tma-app"><small className="text-light" style={{fontSize: '14px'}}>TeamMember_allocation_App</small></Link>
                 </li>
-                
+                <li class="nav-item">
+                <Link class="navbar-brand" to="/group-team-members"><small className="text-light" style={{fontSize: '14px'}}>Group_Team_Member</small></Link>
+                </li>
             </ul>
             </div>
+            
         </div>
         </nav>
     )
